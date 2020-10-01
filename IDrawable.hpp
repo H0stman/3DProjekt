ab16efc5cb0f36ad9e0256a01589d35dbc2df551
Hostman68 <oscarmilstein48@gmail.com>
2020-10-01 20:15:06 +0200

    Worked on terrain class

--- IDrawable.hpp
+++ IDrawable.hpp
@@ -3,8 +3,18 @@
 
 class IDrawable
 {
+protected:
+	struct vertex
+	{
+		XMFLOAT3 position;
+		XMFLOAT2 texturecoordinate;
+		XMFLOAT3 normal;
+		XMFLOAT3 tangent;
+		XMFLOAT3 bitangent;
+	};
 
 public:
+	virtual ~IDrawable();
 	virtual void PrimePipeline(UINT pipelinesettings) = 0;
 };
 
