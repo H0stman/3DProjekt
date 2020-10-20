76fc5878b46c8656268f367154649be040ce6e03
ove1kenobi <ove.odegard@gmail.com>
2020-10-20 13:21:27 +0200

    textures load not tested

--- Model.hpp
+++ Model.hpp
@@ -5,10 +5,15 @@
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
 
+
 #include "IDrawable.hpp"
+#include "Texture.hpp"
 
 class Model : public IDrawable
 {
+	enum textype {diffuse, displacement, normalmap};
+	Texture *texture[3];
+	
 	Model() = delete;
 public:
 	Model(std::string file, ID3D11Device* device);
